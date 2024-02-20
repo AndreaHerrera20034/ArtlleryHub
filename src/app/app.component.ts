6cdff832f6d9a27b0781583ff605609b562c0d2f
AndreaHerrera20034 <106893451+AndreaHerrera20034@users.noreply.github.com>
2024-02-20 17:00:57 -0600

    Proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, AboutComponent, ProjectsComponent, CreateComponent, ContactComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ModuleWithProviders } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {  path: '', component: AboutComponent },
+    { path: 'Sobre mi', component: AboutComponent },
+    { path: 'Proyectos', component: ProjectsComponent },
+    { path: 'Crear proyecto', component: CreateComponent },
+    { path: 'Contacto', component: ContactComponent },
+    { path: '**', component: Error }
+];
