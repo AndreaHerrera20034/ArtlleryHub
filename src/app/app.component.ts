d2fc9fa7b132a2563cbc0fc5c5d1c86aa2e3a475
AndreaHerrera20034 <106893451+AndreaHerrera20034@users.noreply.github.com>
2024-02-21 12:55:46 -0600

    Actualización

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-import { CreateComponent } from './components/create/create.component';
 import { HomeComponent } from './components/Home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CategoryComponent } from './components/category/category.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @Component({
   selector: 'app-root',
@@ -14,9 +16,11 @@ import { FooterComponent } from './components/footer/footer.component';
     HeaderComponent, 
     FooterComponent,
     HomeComponent,
-    AboutComponent, 
-    ProjectsComponent, 
-    CreateComponent, 
+    AboutComponent,
+    CategoryComponent,
+    LoginComponent,
+    SignupComponent,
+    ErrorComponent,
     RouterOutlet
   ],
   templateUrl: './app.component.html',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,17 @@
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-import { CreateComponent } from './components/create/create.component';
 import { HomeComponent } from './components/Home/home.component';
 import { ModuleWithProviders } from '@angular/core';
+import { CategoryComponent } from './components/category/category.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 export const routes: Routes = [
     {  path: '', component: HomeComponent },
     { path: 'Home', component: HomeComponent },
-    { path: 'Sobre mi', component: AboutComponent },
-    { path: 'Proyectos', component: ProjectsComponent },
-    { path: 'Crear proyecto', component: CreateComponent },
+    { path: 'Category', component: CategoryComponent },
+    { path:'About', component: AboutComponent},
+    { path: "Login", component: LoginComponent },
+    { path:"Sign up", component: SignupComponent },
     { path: '**', component: Error }
 ];
