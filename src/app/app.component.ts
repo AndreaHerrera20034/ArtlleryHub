42b3b95454ac7ce6abbbf2f34883d92702b617c1
AndreaHerrera20034 <106893451+AndreaHerrera20034@users.noreply.github.com>
2024-02-21 12:22:10 -0600

    Actualización

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,22 @@ import { RouterOutlet } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { CreateComponent } from './components/create/create.component';
-import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/Home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, AboutComponent, ProjectsComponent, CreateComponent, ContactComponent, RouterOutlet],
+  imports: [
+    HeaderComponent, 
+    FooterComponent,
+    HomeComponent,
+    AboutComponent, 
+    ProjectsComponent, 
+    CreateComponent, 
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { CreateComponent } from './components/create/create.component';
-import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/Home/home.component';
 import { ModuleWithProviders } from '@angular/core';
 
 export const routes: Routes = [
-    {  path: '', component: AboutComponent },
+    {  path: '', component: HomeComponent },
+    { path: 'Home', component: HomeComponent },
     { path: 'Sobre mi', component: AboutComponent },
     { path: 'Proyectos', component: ProjectsComponent },
     { path: 'Crear proyecto', component: CreateComponent },
-    { path: 'Contacto', component: ContactComponent },
     { path: '**', component: Error }
 ];
